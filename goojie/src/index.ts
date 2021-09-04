c9574add31d09a13ffbb89b16aae05bc4ec6a580
princejoogie <princejoogie@gmail.com>
2021-09-04 20:49:46 +0800

    refactored and added user reply when leveling up

--- goojie/src/index.ts
+++ goojie/src/index.ts
@@ -4,7 +4,7 @@ import FS from "fs";
 import { prefix } from "../config.json";
 import { BASE_DIR } from "../constants";
 import { db, timestamp, toServerDate } from "./utils/firebase";
-import { getXpNeeded } from "./utils/magicalFunction";
+import { getTimeDifference, giveXp } from "./utils/magicalFunction";
 const client = new Client();
 const dotenv = require("dotenv");
 dotenv.config();
@@ -31,8 +31,23 @@ client.once("ready", () => {
 });
 
 client.on("message", (msg) => {
-  // TEMPORARY SET LEVEL UP LOGIC HERE
   if (msg.content.startsWith(prefix) && !msg.author.bot) {
+    // HANDLING OF COMMANDS
+    let args = msg.content.split(" ");
+    const cmd = args[0].substring(1, args[0].length);
+
+    if (commands.includes(cmd)) {
+      args.splice(0, 1);
+      const useCommand = require(path.join(
+        BASE_DIR,
+        "src",
+        "commands",
+        `${cmd}${extension}`
+      ));
+      useCommand(msg, args);
+    }
+  } else {
+    // HANDLING OF LEVEL UPS
     const guildRef = db.collection("guilds").doc(msg.guild?.id);
     const userRef = guildRef.collection("users").doc(msg.author.id);
 
@@ -45,69 +60,29 @@ client.on("message", (msg) => {
 
     userRef.get().then((snap) => {
       if (snap.exists) {
+        // existing member
         const data = snap.data();
         if (data) {
           const { level, xp, lastXpGain } = data;
 
           const dbDate = new Date(lastXpGain.toDate());
           const currDate = toServerDate(new Date()).toDate();
-          const difference = Math.abs(
-            (currDate.getTime() - dbDate.getTime()) / 1000
-          );
+          const difference = getTimeDifference({
+            start: dbDate,
+            end: currDate,
+          });
 
           if (difference > 10) {
-            const xpGain = Math.floor(Math.random() * 10) + 15;
-            getXpNeeded(level, xp).then((xpNeeded) => {
-              console.log("------------------------");
-              console.log({ difference: `${difference}sec` });
-              console.log(`+${xpGain}xp. new xp: ${xp + xpGain}`);
-              if (xpGain >= xpNeeded) {
-                console.log(`leveled up. new level: ${level + 1}`);
-                userRef.update({
-                  level: level + 1,
-                  xp: xp + xpGain,
-                  lastXpGain: timestamp(),
-                });
-              } else {
-                userRef.update({
-                  xp: xp + xpGain,
-                  lastXpGain: timestamp(),
-                });
-              }
-              console.log("------------------------");
-            });
+            giveXp({ level, xp, userRef, msg });
           } else {
             console.log("can only level up once per minute");
           }
         }
       } else {
-        userRef.set({
-          level: 0,
-          xp: 0,
-          lastXpGain: timestamp(),
-        });
+        // new member
+        giveXp({ level: 0, xp: 0, userRef, msg });
       }
     });
-
-    // msg.reply(`Guild ID: ${msg.guild?.id}`);
-    // msg.reply(`Channel ID: ${msg.channel?.id}`);
-    // let args = msg.content.split(" ");
-    // const cmd = args[0].substring(1, args[0].length);
-
-    // if (commands.includes(cmd)) {
-    //   args.splice(0, 1);
-    //   const useCommand = require(path.join(
-    //     BASE_DIR,
-    //     "src",
-    //     "commands",
-    //     `${cmd}${extension}`
-    //   ));
-    //   useCommand(msg, args);
-    // }
-  }
-  // handle level ups
-  else {
-    // TODO
   }
 });
 
--- goojie/src/utils/magicalFunction.ts
+++ goojie/src/utils/magicalFunction.ts
@@ -1,16 +1,20 @@
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+/*
+ * NOTE: pure node tests
+ * const readline = require("readline").createInterface({
+ *   input: process.stdin,
+ *   output: process.stdout,
+ * });
+ * const input = async (msg: string): Promise<string> => {
+ *   return await new Promise((resolve) =>
+ *     readline.question(msg, (result: string) => resolve(result))
+ *   );
+ * };
+ *   const currentLevel = +(await input("What is you current level? "));
+ *   const currentXp = +(await input("What is you current xp? "));
+ */
 
-// const input = async (msg: string): Promise<string> => {
-//   return await new Promise((resolve) =>
-//     readline.question(msg, (result: string) => resolve(result))
-//   );
-// };
-
-//   const currentLevel = +(await input("What is you current level? "));
-//   const currentXp = +(await input("What is you current xp? "));
+import { timestamp, firebase } from "./firebase";
+import { Message } from "discord.js";
 
 export const getXpNeeded = async (currentLevel: number, currentXp: number) => {
   const xpToLevel =
@@ -20,4 +24,47 @@ export const getXpNeeded = async (currentLevel: number, currentXp: number) => {
   const xpNeeded = xpToLevel - currentXp;
 
   return xpNeeded;
+};
+
+interface GetTimeDifferenceProps {
+  start: Date;
+  end: Date;
+}
+
+export const getTimeDifference = ({ start, end }: GetTimeDifferenceProps) => {
+  const difference = Math.abs((end.getTime() - start.getTime()) / 1000);
+  return difference;
+};
+
+interface GiveXpProps {
+  level: number;
+  xp: number;
+  userRef: firebase.firestore.DocumentReference<firebase.firestore.DocumentData>;
+  msg: Message;
+}
+
+export const giveXp = async ({ level, xp, userRef, msg }: GiveXpProps) => {
+  const xpGain = Math.floor(Math.random() * 10) + 15;
+  const xpNeeded = await getXpNeeded(level, xp);
+
+  let newLevel = level;
+  let newXp = xp + xpGain;
+
+  if (xpGain >= xpNeeded) {
+    newLevel = level + 1;
+    msg.channel.send(
+      `NICE ${msg.author}, you increased you programming skills to level ${newLevel}!`
+    );
+  }
+
+  console.log({ newLevel, newXp });
+
+  userRef.set(
+    {
+      level: newLevel,
+      xp: newXp,
+      lastXpGain: timestamp(),
+    },
+    { merge: true }
+  );
 };
